849b36d66b83b454d3b783dddc1f9f193c3e56c8
jsdev <js.dev.sanchez@gmail.com>
2024-02-23 20:38:45 -0300

    hotfix: size image of cards

--- src/app/components/MainSection.tsx
+++ src/app/components/MainSection.tsx
@@ -6,6 +6,6 @@ export default function RootLayout({
     children: React.ReactNode;
   }>) {
   return (
-     <main className="h-screen flex-1 px-28 py-8 phone:grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-3 desktop:grid-cols-4">{children}</main>
+     <main className="h-screen flex-1 pl-28 pr-28 py-8 sm-phone:pr-2 phone:pr-2 xl-phone:pr-2 sm-phone:pl-24 phone:pl-24 xl-phone:pl-24 phone:grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-3 desktop:grid-cols-4">{children}</main>
   )
 }
